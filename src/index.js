d5cd5bd955428f6250a65aad44d1e851c1239d25
Viktor <65297330+LOOKATmixer@users.noreply.github.com>
2023-05-22 19:58:44 +0500

    развернул Webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var o = "padding: 10px; color: #fff; background-color: #ff2c2c; border-radius: 10px;",
+    b = "padding: 20px 0; color: #fff; background-color: rgba(255, 255, 255 0);",
+    a = "padding: 10px 0; color: #fff; background-color: rgba(255, 255, 255 0);",
+    c = "padding: 20px; color: #fff; background-color: #000;";
+console.log("%cDeveloped by", a), console.log("%c@mynumbernine", o), console.log("%chttps://t.me/mynumbernine", b);
+
+import './style.css'
